2a610de9f00a3967945f3bb13ad2e1981ef78045
Adam Fragkiadakis <adamfrag@gmail.com>
2021-12-29 18:03:25 +0200

    refactor ca routing

--- app/ca/views.py
+++ app/ca/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, flash, request
+from flask import render_template, session, flash, request, redirect, url_for
 from flask_login import login_required
 
 from . import ca
@@ -7,42 +7,17 @@ from .. import db
 from ..models import Member
 
 
+@ca.route('/')
+@login_required
+def ca_home():
+    return redirect(url_for('ca.dashboard'))
+
+
 @ca.route('/dashboard')
 @login_required
 def dashboard():
     return render_template('private/dashboard.html',
                            member={'id': session['_user_id'], 'username': session['_username']}, title="Dashboard")
-
-
-@ca.route('/profile/<int:member_id>', methods=['GET', 'POST'])
-@login_required
-def profile(member_id):
-    form = ProfileForm()
-    member = Member.query.get_or_404(member_id)
-
-    if form.validate_on_submit():
-        if not member.verify_password(form.confirm_changes.data):
-            flash('Invalid Password')
-            return render_template('private/profile.html', form=form, member=member, title='Profile')
-
-        if form.password.data:
-            member.password = form.password.data
-
-        # Update member properties
-        member.first_name = form.first_name.data
-        member.last_name = form.last_name.data
-        member.username = form.username.data
-        member.email = form.email.data
-        member.telephone = form.telephone.data
-        member.city = form.city.data
-        member.address = form.address.data
-
-        # Update database
-        db.session.commit()
-
-        flash('You have successfully updated your profile.')
-
-    return render_template('private/profile.html', form=form, member=member, title='Profile')
 
 
 @ca.route('/members', methods=['GET', 'POST'])
@@ -75,3 +50,34 @@ def list_members():
 
     return render_template('private/members.html', member={'id': session['_user_id'], 'username': session['_username']},
                            members=members, title='Members')
+
+
+@ca.route('/members/<int:member_id>', methods=['GET', 'POST'])
+@login_required
+def profile(member_id):
+    form = ProfileForm()
+    member = Member.query.get_or_404(member_id)
+
+    if form.validate_on_submit():
+        if not member.verify_password(form.confirm_changes.data):
+            flash('Invalid Password')
+            return render_template('private/profile.html', form=form, member=member, title='Profile')
+
+        if form.password.data:
+            member.password = form.password.data
+
+        # Update member properties
+        member.first_name = form.first_name.data
+        member.last_name = form.last_name.data
+        member.username = form.username.data
+        member.email = form.email.data
+        member.telephone = form.telephone.data
+        member.city = form.city.data
+        member.address = form.address.data
+
+        # Update database
+        db.session.commit()
+
+        flash('You have successfully updated your profile.')
+
+    return render_template('private/profile.html', form=form, member=member, title='Profile')

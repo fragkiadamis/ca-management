17af7f8d3f870634407758f5201cb15b1b3eedd6
Adam Fragkiadakis <adamfrag@gmail.com>
2021-12-29 19:51:53 +0200

    delete unnecessary js file

--- app/static/js/request.js
+++
@@ -1,9 +0,0 @@
-const addEvents = () => {
-    let status_toggles = document.querySelectorAll('.status-toggle');
-    status_toggles.forEach(toggle => {
-        toggle.addEventListener('change', e => {
-            let form = e.target.closest('form')
-            form.submit();
-        });
-    });
-}

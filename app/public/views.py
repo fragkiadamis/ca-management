4e8b12e6d38b63cf9b124d41708295744e711f7f
Adam Fragkiadakis <adamfrag@gmail.com>
2022-01-28 01:07:35 +0200

    remove static public files

--- app/public/views.py
+++ app/public/views.py
@@ -1,25 +1,11 @@
+from . import home
+
 from flask import render_template
 
-from . import home
-from ..auth.forms import LoginForm
+from app.auth.forms import LoginForm
 
 
 @home.route('/')
 def homepage():
     form = LoginForm()
     return render_template('index.html', form=form, title="Welcome")
-
-
-@home.route('/about')
-def about():
-    return render_template('about.html', title="About")
-
-
-@home.route('/teams')
-def teams():
-    return render_template('teams.html', title="Teams")
-
-
-@home.route('/faq')
-def faq():
-    return render_template('faq.html', title="FAQ")

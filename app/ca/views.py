6645a98ac0f50d00eb8a315844465c969a557c14
Adam Fragkiadakis <adamfrag@gmail.com>
2022-01-29 14:06:44 +0200

    filter dashboard entities

--- app/ca/views.py
+++ app/ca/views.py
@@ -1,8 +1,9 @@
-from flask import render_template, session, flash, redirect, url_for
+from flask import render_template, session, flash, redirect, url_for, request
 from flask_login import login_required
 
 from . import ca
-from ..models import Activity, Announcement
+from ..filters import get_related_entities
+from ..models import Activity, Announcement, Member
 
 
 @ca.route('/')
@@ -14,8 +15,9 @@ def ca_home():
 @ca.route('/dashboard')
 @login_required
 def dashboard():
+    filter_by = request.args.get('filter_by')
+    member = Member.query.get_or_404(int(session['_user_id']))
+    activities, teams = get_related_entities(filter_by, member, ('Admin', 'Editor'), 'activities')
+    announcements, teams = get_related_entities(filter_by, member, ('Admin', 'Editor'), 'announcements')
     sess_user = {'id': session['_user_id'], 'username': session['_username'], 'roles': session['_user_roles']}
-    activities = Activity.query.all()
-    announcements = Announcement.query.all()
-    return render_template('private/dashboard.html', user=sess_user, activities=activities, announcements=announcements,
-                           title="Dashboard")
+    return render_template('private/dashboard.html', user=sess_user, activities=activities, announcements=announcements, teams=teams, title="Dashboard")
